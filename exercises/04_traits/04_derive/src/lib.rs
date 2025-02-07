93f984f100a7949280b84f7ad1f6b76151bebf2d
4rjunc <arjunmundamani@gmail.com>
2025-02-07 19:23:49 +0530

    some

--- exercises/04_traits/04_derive/src/lib.rs
+++ exercises/04_traits/04_derive/src/lib.rs
@@ -7,6 +7,7 @@
 // `assert_eq!` requires `Ticket` to implement `Debug` because, when the assertion fails, it tries to
 // print both sides of the comparison to the terminal.
 // If the compared type doesn't implement `Debug`, it doesn't know how to represent them!
+// If the compared type doesn't implement `Debug`, it doesn't know how to represent them!
 #[derive(Debug, PartialEq)]
 struct Ticket {
     title: String,
